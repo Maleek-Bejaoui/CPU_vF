e02c03de50273c5fb0691a1752ba7bc81526ca8f
Maleek <101218361+Maleek-Bejaoui@users.noreply.github.com>
2025-04-13 23:04:56 +0200

    Update top.v

--- src/top.v
+++ src/top.v
@@ -17,14 +17,16 @@ module tt_um_top (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-    assign uo_out[7:1]  = 6'b000000;  // Example: ou_out is the sum of ui_in and uio_in
+//  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
     assign uio_out = 0;
     assign uio_oe  = 0;
+    assign uo_out[7:1]  = 7'b0000000;
+
+    
 
   // List all unused inputs to prevent warnings
-//  wire _unused = &{ena, clk, rst_n, 1'b0};
-//  wire _unused = &{ena, 1'b0};
-
+    wire _unused = &{ui_in[7:2], 6'b000000};
+    wire _unused1 = &{uio_in, 8'b0};
 
 
  CPU_Bootloader m_cpu
