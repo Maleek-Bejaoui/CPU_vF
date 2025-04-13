b8a574fa60d2f1d61c930c065d67f937c82e1268
Maleek <101218361+Maleek-Bejaoui@users.noreply.github.com>
2025-04-13 23:35:12 +0200

    Update ram.v

--- src/ram.v
+++ src/ram.v
@@ -1,43 +1,21 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 04/07/2025 04:15:15 PM
-// Design Name: 
-// Module Name: ramverilog
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 module RAM_SP_64_8 (
-    input [5:0] add,        // Adresse (6 bits)
-    input [15:0] data_in,   // Données à écrire
-    input r_w,              // Signal de lecture (r_w = 0) / écriture (r_w = 1)
-    input enable,           // Activation du module
-    input clk,              // Horloge
-    input ce,               // Signal d'activation du processus
-    output reg [15:0] data_out // Données lues
+    input [5:0] add,        
+    input [15:0] data_in,   
+    input r_w,             
+    input enable,           
+    input clk,             
+    input ce,               
+    output reg [15:0] data_out 
 );
 
-    // Déclaration de la mémoire
     reg [15:0] memory [0:63];  // RAM de 64 mots de 16 bits
 
     always @(posedge clk) begin
         if (ce) begin
             if (enable) begin
-                if (r_w == 1'b0) begin // Lecture
+                if (r_w == 1'b0) begin 
                     data_out <= memory[add];
-                end else begin // Écriture
+                end else begin 
                     memory[add] <= data_in;
                 end
             end
