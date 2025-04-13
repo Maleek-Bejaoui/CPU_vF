363da1f57811cb6a0da9512df987783f7387b298
Maleek <101218361+Maleek-Bejaoui@users.noreply.github.com>
2025-04-14 00:09:33 +0200

    Update cpu_bootloader.v

--- src/cpu_bootloader.v
+++ src/cpu_bootloader.v
@@ -1,14 +1,14 @@
 module CPU_Bootloader (
-    input clk,                // Horloge
-    input rst,                // Reset
-    input ce,                 // Signal d'activation
-    input scan_memory,       // Scan mémoire
-    input rx,                 // Réception de données série
-    output tx                 // Transmission de données série
+    input clk,                
+    input rst,                
+    input ce,                
+    input scan_memory,       
+    input rx,                 
+    output tx                 
 );
 
-    // Composants internes
-    wire [15:0] cpu_ram_data_out,boot_ram_in;  
+    
+    wire [15:0] boot_ram_in;  
     wire [5:0] sig_adr, boot_ram_adr;
     wire carry, clear_carry, enable_mem, load_R1, load_accu, load_carry, w_mem;
     wire [2:0] sel_UAL_UT, sel_UAL_UC;
@@ -18,14 +18,14 @@ module CPU_Bootloader (
     wire [15:0] sig_ram_in;
     wire boot, boot_ram_rw, boot_ram_enable;
     
-    // Composants
+  
     Control_Unit UC (
         .clk(clk),
         .ce(ce),
         .rst(rst),
         .carry(carry),
         .boot(boot),
-        .data_in(cpu_ram_data_out),  // Utilisation de cpu_ram_data_out
+        .data_in(ram_data_out),  
         .adr(sig_adr),
         .clear_carry(clear_carry),
         .enable_mem(enable_mem),
@@ -39,7 +39,7 @@ module CPU_Bootloader (
     assign sel_UAL_UT = sel_UAL_UC;
 
     UT UT1 (
-        .data_in(cpu_ram_data_out),  
+        .data_in(ram_data_out),  
         .clk(clk),
         .ce(ce),
         .rst(rst),
@@ -67,13 +67,13 @@ module CPU_Bootloader (
         .ram_in(boot_ram_in)
     );
 
-    // MUX pour le contrôle de la RAM
+   
     assign sig_rw = (boot == 1'b1) ? boot_ram_rw : w_mem;
     assign sig_ram_enable = (boot == 1'b1) ? boot_ram_enable : enable_mem;
     assign sig_ram_adr = (boot == 1'b1) ? boot_ram_adr : sig_adr;
     assign sig_ram_in = (boot == 1'b1) ? boot_ram_in : UT_data_out;
 
-    // RAM
+   
     RAM_SP_64_8 UM (
         .add(sig_ram_adr),
         .data_in(sig_ram_in),
@@ -81,7 +81,7 @@ module CPU_Bootloader (
         .enable(sig_ram_enable),
         .clk(clk),
         .ce(ce),
-        .data_out(cpu_ram_data_out)  
+        .data_out(ram_data_out)  
     );
 
 endmodule
